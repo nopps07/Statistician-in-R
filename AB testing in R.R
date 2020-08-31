3f86b7751f80c43849501fd4a028b2e436561ff5
Gunho Lee <gunholee0329@gmail.com>
2020-08-31 08:08:44 +0200

    Start

---
+++ AB testing in R.R
@@ -0,0 +1,9 @@
+#A/B Testing in R
+
+#Conversion rate
+
+#Test1
+#Question. Will changing the homepage photo result in more "Adopt today' clicks?
+#Hyphothesis: seeing a cat in a hat will make people more likely to want to adopt.
+#Dependent variable: Clicked "Adopt Today!" button or not.
+#Independent variable: Homepage photo
