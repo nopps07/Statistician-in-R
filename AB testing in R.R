ecd745ab873fd855473a292ff3992060b1f6c37a
Gunho Lee <gunholee0329@gmail.com>
2020-09-20 20:42:25 +0200

    # Chapter 3

--- AB testing in R.R
+++ AB testing in R.R
@@ -255,3 +255,119 @@ followup_experiment_sep_results <- glm(clicked_adopt_today ~ condition,
                                        family = "binomial",
                                        data = followup_experiment_data_sep) %>%
   tidy()
+
+
+## Chapter 3
+#A/B testing 
+
+# Compute summary by month
+viz_website_2017 %>%
+  group_by(month(visit_date)) %>%
+  summarise(article_conversion_rate = mean(clicked_article))
+
+# Compute 'like' click summary by month
+viz_website_2017_like_sum <- viz_website_2017 %>%
+  mutate(month = month(visit_date, label = TRUE)) %>%
+  group_by(month) %>%
+  summarize(like_conversion_rate = mean(clicked_like))
+
+# Plot 'like' click summary by month
+ggplot(viz_website_2017_like_sum,
+       aes(x = month, y = like_conversion_rate, group = 1)) +
+  geom_point() +
+  geom_line() +
+  scale_y_continuous(limits = c(0, 1), labels = percent)
+
+# Plot comparison of 'like'ing and 'sharing'ing an article
+ggplot(viz_website_2017_like_share_sum,
+       aes(x = month, y = conversion_rate, color = action, group = action)) +
+  geom_point() +
+  geom_line() +
+  scale_y_continuous(limits = c(0, 1), labels = percent)
+
+
+#Types of A/B Testing
+#A/B - compare a control and a test condition ("Tips" vs "Tools.")
+#A/A - compare two groups of control conditions ("Tips (Group 1)" to "Tips (Group 2)")
+#A/B/N - compare a control condition to any number of different test conditions (e.g. "Tips" vs "Tools" vs "strategies")
+
+# Compute conversion rates for A/A experiment
+viz_website_2018_01_sum <- viz_website_2018_01 %>%
+  group_by(condition) %>%
+  summarize(like_conversion_rate = mean(clicked_like))
+
+viz_website_2018_01_sum
+
+# Plot conversion rates for two conditions
+ggplot(viz_website_2018_01_sum,
+       aes(x = condition, y = like_conversion_rate)) +
+  geom_bar(stat = "identity") +
+  scale_y_continuous(limits = c(0, 1), labels = percent)
+
+# Run logistic regression
+aa_experiment_results <- glm(clicked_like ~ condition, #condition is independent
+                             family = "binomial",
+                             data = viz_website_2018_01) %>%
+  tidy()
+aa_experiment_results
+
+# Confounding variabless
+# it is an element of the environment that could affect your ability to find out the truth of an A/B experiment.
+# External factor? white colour goes up since Snow White is on cinema
+
+# Compute 'like' conversion rate by week and condition
+viz_website_2018_02 %>%
+  mutate(week = week(visit_date)) %>%
+  group_by(week, condition) %>%
+  summarize(like_conversion_rate = mean(clicked_like))
+
+# Compute 'like' conversion rate by if article published and condition
+viz_website_2018_02 %>%
+  group_by(article_published, condition) %>%
+  summarize(like_conversion_rate = mean(clicked_like))
+
+
+# Plot 'like' conversion rates by date for experiment
+ggplot(viz_website_2018_02_sum,
+       aes(x = visit_date,
+           y = like_conversion_rate,
+           color = condition,
+           linetype = article_published,
+           group = interaction(condition, article_published))) +
+  geom_point() +
+  geom_line() +
+  geom_vline(xintercept = as.numeric(as.Date("2018-02-15"))) +
+  scale_y_continuous(limits = c(0, 0.3), labels = percent)
+# Question
+# Find the date when the article was first published. Now add that date as the value for xintercept in geom_vline(). (Note: it should be of the format "YYYY-MM-DD".)
+
+# Side effects
+# Examples:
+# load times, information 'above the fold'
+# Confounding variables VS Side effects
+# When choosing your pictures you introduced a confounding variable of cat age,
+# when adding the pictures you had a side effect of load times.
+
+# Compute 'like' conversion rate and mean pageload time by day
+viz_website_2018_03_sum <- viz_website_2018_03 %>%
+  group_by(visit_date, condition) %>%
+  summarize(mean_pageload_time = mean(pageload_time),
+            like_conversion_rate = mean(clicked_like))
+head(viz_website_2018_03_sum)
+
+# Plot effect of 'like' conversion rate by pageload time
+ggplot(viz_website_2018_03_sum,
+       aes(x = mean_pageload_time, y = like_conversion_rate, color = condition)) +
+  geom_point()
+
+# Plot 'like' conversion rate by day
+ggplot(viz_website_2018_03_sum,
+       aes(x = visit_date,
+           y = like_conversion_rate,
+           color = condition,
+           linetype = pageload_delay_added,
+           group = interaction(condition, pageload_delay_added))) +
+  geom_point() +
+  geom_line() +
+  geom_vline(xintercept = as.numeric(as.Date("2018-03-15"))) +
+  scale_y_continuous(limits = c(0, 0.3), labels = percent)
